1b1bf2cedf477580ea8138664eba8316213d4143
hannah-frank <hannah.frank@student.uni-tuebingen.de>
2018-05-30 13:35:14 +0200

    constructor

--- src/progproj/homework/Read.java
+++ src/progproj/homework/Read.java
@@ -6,18 +6,18 @@ import java.util.ArrayList;
 public class Read extends FastAEntry{
 
     //private String header; //inherited from superclass FastAEntry
-    private String sequence; //inherited from superclass FastAEntry - TODO: should be in superclass??
+    //private String sequence; //inherited from superclass FastAEntry - should be in superclass??
     private String id; //id out of header
     private List<GffEntry> gffEntries;//list of GFF entries
-    //jo
-    //TODO: construktor??
-    public Read(){
-        //super();
+
+    public Read(FastAEntry fastaEntry){
+        super(fastaEntry.getHeader(),fastaEntry.getSequence());
+        this.id = getIdFromHeader(fastaEntry.getHeader());
         //initialize gffEntries list, so that we won't have to check if its null all the time
         this.gffEntries = new ArrayList<GffEntry>();
     }
 
-    public String getSequence(){return this.sequence;} //TODO: should be in superclass??
+    //public String getSequence(){return this.sequence;} //should be in superclass??
     public String getId(){
         return this.id;
     }
@@ -25,8 +25,8 @@ public class Read extends FastAEntry{
         return this.gffEntries;
     }
     
-    //TODO: remove unnecessary setters?
-    public void setSequence(String sequence){this.sequence = sequence;} //TODO: should be in superclass??
+    //remove unnecessary setters?
+    //public void setSequence(String sequence){this.sequence = sequence;} //should be in superclass??
     public void setId(String id){
         this.id = id;
     }
@@ -34,10 +34,11 @@ public class Read extends FastAEntry{
         this.gffEntries = gffEntries;
     }
 
-    //processes id out of the header; id = first in header until first whitespace
-    public String getIdFromHeader(){
-        this.id = this.getHeader().split("/s")[0]; //splits at whitespace & extracts first 'word'
-        return this.id;
+    //processes id out of the header; id = first in header until first whitespace & delete first sign <
+    private String getIdFromHeader(String header){
+        //String id = header.split("/s")[0];
+        String id = "";
+        return id;
     }
 
     //Added ein Gff Entry
