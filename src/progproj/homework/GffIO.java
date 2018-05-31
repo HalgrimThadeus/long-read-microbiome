66db8c2d75a1d2a8c2fee62683ee2c95978a3d46
rabeUni <rabanus.derr@student.uni-tuebingen.de>
2018-05-31 23:46:22 +0200

    meaningless changes

--- src/progproj/homework/GffIO.java
+++ src/progproj/homework/GffIO.java
@@ -1,6 +1,8 @@
 package progproj.homework;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
