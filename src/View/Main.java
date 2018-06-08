f6ff4e3c2a74712541ae7a6a22e72a2533290be2
rabeUni <rabanus.derr@student.uni-tuebingen.de>
2018-06-08 21:43:44 +0200

    moved Gui resource to res folder

--- src/View/Main.java
+++ src/View/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../../res/GUI/sample.fxml"));
         primaryStage.setTitle("DNA to protein sequence");
         primaryStage.setScene(new Scene(root, 600, 250));
         primaryStage.show();
