c11e3563d4494fb4b178899e160d048ad69e216f
hannah-frank <hannah.frank@student.uni-tuebingen.de>
2018-06-08 19:17:38 +0200

    added View (again)

---
+++ src/View/Main.java
@@ -0,0 +1,23 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("DNA to protein sequence");
+        primaryStage.setScene(new Scene(root, 600, 250));
+        primaryStage.show();
+    }
+
+}
