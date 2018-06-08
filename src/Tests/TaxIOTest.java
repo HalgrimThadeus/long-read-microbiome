e276b6d14cfee01dab5af056972f6cd937ff839e
rabeUni <rabanus.derr@student.uni-tuebingen.de>
2018-06-08 21:07:22 +0200

    raw test classes

---
+++ src/Tests/TaxIOTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaxIOTest {
+
+    //todo: for rabeUni
+    //plus adding test data in resources
+    @Test
+    public void readInTaxTree() {
+    }
+}
---
+++ src/Tests/TaxTreeTest.java
@@ -0,0 +1,30 @@
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaxTreeTest {
+
+    //todo: for rabeUni
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void setNameOfId() {
+    }
+
+    @Test
+    public void setChildren() {
+    }
+
+    @Test
+    public void getAllChildren() {
+    }
+
+    @Test
+    public void getId() {
+    }
+}
