22a6e9718dbd76dcdc268d172695a79f2f8750c9
Patrick Schirm <patrick.schirm@codenic.de>
2018-06-10 23:08:25 +0200

    updated FastAIO

--- src/Model/IO/FastAIO.java
+++ src/Model/IO/FastAIO.java
@@ -16,12 +16,6 @@ import java.util.List;
  */
 public class FastAIO {
 
-    /*TODO Sollte wie in der GffIO nur die FastA Datei lesen/schreiben und eine Liste von fastAEntrys zurückgegeben.
-      TODO Geter können entfernt werden, da diese bereits in FastAEntry sind!
-     */
-    private List<String> headerList;
-    private List<String> sequenceList;
-
     /**
      * For new FastaA which will be filled with the add method
      */
@@ -49,52 +43,13 @@ public class FastAIO {
     public FastAIO(String path) throws IOException {
         readFastA(path);
     }
-
-    //TODO Modifizieren das FastAEntry erzeugt und zurückegeben werden
     /**
      * With a given Reader this method is creating 2 lists with the input from the reader.
      * Split up into header and sequence lists.
      *     * @param r
      * @throws IOException
      */
-    //Bereits modifiziert gibt Liste zurück
-    /*
-    public void readFastA(Reader r) throws IOException {
-        String thisLine;
-        String nextLine;
-        String accumulator = "";
 
-        BufferedReader reader = new BufferedReader(r);
-
-        if ( (thisLine = reader.readLine()) != null) {
-            nextLine = reader.readLine();
-            while (nextLine != null) {
-                if ( thisLine.startsWith(">") && nextLine.startsWith(">")) {
-                    thisLine = nextLine;
-                    nextLine = reader.readLine();
-                } else if ( thisLine.startsWith(">") && !(nextLine.startsWith(">"))) {
-                    headerList.add(thisLine);
-                    thisLine = nextLine;
-                    nextLine = reader.readLine();
-                } else if ( !(thisLine.startsWith(">")) && !(nextLine.startsWith(">"))) {
-                    accumulator += thisLine;
-                    thisLine = nextLine;
-                    nextLine = reader.readLine();
-                } else {
-                    accumulator += thisLine;
-                    sequenceList.add(accumulator);
-                    accumulator = "";
-                    thisLine = nextLine;
-                    nextLine = reader.readLine();
-                }
-            }
-            accumulator += thisLine;
-            sequenceList.add(accumulator);
-        }
-
-        reader.close();
-    }
-    */
     //TODO make it better, was from error fixing
     public List<FastAEntry> readFastA(Reader r) throws IOException{
         String thisLine;
@@ -151,14 +106,15 @@ public class FastAIO {
      * @param w
      * @throws IOException
      */
-    public void writeFastA(Writer w) throws IOException {
+    // not jet tested
+    public void writeFastA(Writer w, List<FastAEntry> outputList) throws IOException {
 
         BufferedWriter writer = new BufferedWriter(w);
 
-        for(int i = 0; i < headerList.size(); i++){
-            writer.write(headerList.get(i));
+        for(int i = 0; i < outputList.size(); i++){
+            writer.write(outputList.get(i).getHeader());
             writer.newLine();
-            writer.write(sequenceList.get(i));
+            writer.write(outputList.get(i).getSequence());
             writer.newLine();
         }
         writer.flush();
@@ -171,59 +127,8 @@ public class FastAIO {
      * @param path
      * @throws IOException
      */
-    public void writeFastA(String path) throws IOException {
+    public void writeFastA(String path, List<FastAEntry> outputList) throws IOException {
         Writer writer = new FileWriter(path);
-        writeFastA(writer);
-    }
-
-    //TODO getter können eigentlich alle entfernt werden, da diese schon in FastAEntrys
-
-    /**
-     * is giving the header at given position
-     *
-     * @param i
-     * @return
-     * @throws Exception
-     */
-    public String getSequence(int i) throws Exception {
-        if (sequenceList.size() < i -1) {
-            throw new Exception("No Such Sequence");
-        }
-        return sequenceList.get(i - 1);
-    }
-
-    /**
-     * is giving the header at given position
-     *
-     * @param i
-     * @return
-     * @throws Exception
-     */
-    public String getHeader(int i) throws Exception {
-        if (headerList.size() < i -1) {
-            throw new Exception("No such Header");
-        }
-        return headerList.get(i - 1);
-    }
-
-
-    /**
-     * returns the size of the header list
-     *
-     * @return
-     */
-    public int getSize(){
-        return (headerList.size());
-    }
-
-    /**
-     * adds header and sequence at the end of the corresponding lists
-     *
-     * @param header
-     * @param sequence
-     */
-    public void add(String header, String sequence) {
-        headerList.add(header);
-        sequenceList.add(sequence);
+        writeFastA(writer, outputList);
     }
 }
