7b1d352eb5dddd8a4c7bcfd5622fe9309bd6168d
hannah-frank <hannah.frank@student.uni-tuebingen.de>
2018-06-13 16:03:30 +0200

    idea for filterview (just idea, wrong package,classes,... and no functionality)

--- src/View/Main.java
+++ src/View/Main.java
@@ -14,9 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/sampleP.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/filterview.fxml"));
         primaryStage.setTitle("DNA to protein sequence");
-        primaryStage.setScene(new Scene(root, 600, 250));
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
