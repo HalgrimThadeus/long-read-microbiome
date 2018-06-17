15787a78b97d1b14ae5e5e7de67ecbfb481d09d9
rabeUni <rabanus.derr@student.uni-tuebingen.de>
2018-06-17 22:00:08 +0200

    added file opening functionality (ONLY TESTING)

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,12 +1,32 @@
 package View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.*;
+
 public class Main extends Application {
+
+    //todo: these attributes must be moved to controller
+    private @FXML
+    Button openFileButton;
+    private @FXML
+    TextField outputTestField;
+    private FileChooser fileChooser;
+
+
+    private Scene scene;
 
     public static void main(String[] args) {
         launch(args);
@@ -14,10 +34,51 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/filterview.fxml"));
+        //set environmental parameters
+        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
         primaryStage.setTitle("DNA to protein sequence");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        scene = new Scene(root, 600, 500);
+
+
+
+        ///////////////////////////////////////////////////////////////////////
+        //test part
+        //set some input interfaces
+
+        fileChooser = new FileChooser();
+        openFileButton = (Button) scene.lookup("#openFileButton");
+        outputTestField = (TextField) scene.lookup("#outputTestField");
+        openFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    try {
+                        openFile(file);
+                    } catch(IOException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        });
+        /////////////////////////////////////////////////////////////////////////
+        //test until here
+
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    /**
+     * test method for opening file
+     * todo: must be moved to controller or model
+     */
+    private void openFile(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        String inputString = reader.readLine();
+
+        outputTestField.setText(inputString);
+    }
 }
