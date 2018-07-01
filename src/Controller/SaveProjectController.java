4f56054322fe641d1d9fe0cc2bec6e07495077fe
d-manuel <manuel.dienert@t-online.de>
2018-07-01 21:51:12 +0200

    Samples Saving now Working!

--- src/Controller/SaveProjectController.java
+++ src/Controller/SaveProjectController.java
@@ -1,4 +1,5 @@
 package Controller;
+
 import Model.IO.ConfigIO;
 import Model.Project;
 import Model.Sample;
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class SaveProjectController {
 
     public void saveProject() throws Exception {
@@ -19,59 +19,60 @@ public class SaveProjectController {
         //get the list of samples and the filepaths of the samples
         List<Sample> listOfSamples = Project.listOfSamples;
 
-        List<File[]> listOfPaths= Project.listOfSamplesFilePaths;
+        List<File[]> listOfPaths = Project.listOfSamplesFilePaths;
 
-        for(File[] file: listOfPaths){
+        File configFileToWrite = createEmptyFile();
+
+        saveFilePathToFile(listOfSamples, listOfPaths, configFileToWrite);
+
+
+    }
+
+    private File createEmptyFile() {
+        File fileToWrite = null;
+
+        //choose the save-directory
+        int configVersionName = 1;
+        JFileChooser f = new JFileChooser();
+        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        f.showSaveDialog(null);
+        System.out.println(f.getSelectedFile());
+
+        //make a config file, that doesnt exist already
+        do {
+            String fileToWritePath = f.getSelectedFile() + "\\config" + configVersionName + ".lrcfg";
+
+            fileToWrite = new File(fileToWritePath);
+
+            configVersionName++;
+
+        } while (fileToWrite.isFile());
+
+        return fileToWrite;
+
+    }
+
+    //Information: FileFormat *.lrcfg stands for "long read config File".
+    public void saveFilePathToFile(List<Sample> listOfSamples, List<File[]> listOfPaths, File configFileToWrite) throws Exception {
+        String stringToWrite = "";
+
+        for (int i = 0; i < listOfPaths.size(); i++) {
             //create the StringFilePath arrays (with the 3 filepaths of fasta, gff, and csv)
+
+            File[] file = listOfPaths.get(i);
+
             String fastaPath = file[0].getAbsolutePath();
             String gffPath = file[1].getAbsolutePath();
             String csvPath = file[2].getAbsolutePath();
 
-            String[] pathsToPass = new String[3];
-            pathsToPass[0] = fastaPath;
-            pathsToPass[1] = gffPath;
-            pathsToPass[2] =csvPath;
-
-            saveFilePathToFile(pathsToPass);
-        }
-    }
-
-    //Information: FileFormat *.lrcfg stands for "long read config File".
-    public void saveFilePathToFile(String[] filePaths) throws Exception {
-        try {
-            File fileToWrite = null;
-
-            //choose the save-directory
-            int configVersionName = 1;
-            JFileChooser f = new JFileChooser();
-            f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            f.showSaveDialog(null);
-            System.out.println(f.getSelectedFile());
-
-            //make a config file, that doesnt exist already
-            do {
-                String fileToWritePath = f.getSelectedFile() + "\\config" + configVersionName + ".lrcfg";
-
-                fileToWrite = new File(fileToWritePath);
-
-                configVersionName++;
-
-            } while (fileToWrite.isFile());
-
-            //
-            ConfigIO WriteConfigIO = new ConfigIO(fileToWrite);
-            String stringToWrite = "#Sample" + "\n";
-            for (int i = 0; i < filePaths.length; i++) {
-                stringToWrite += filePaths[i] + "\n";
-            }
-
-
-            WriteConfigIO.writeToFile(stringToWrite);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            stringToWrite += "Sample" + i + "\n";
+            stringToWrite += fastaPath + "\n";
+            stringToWrite += gffPath + "\n";
+            stringToWrite += csvPath + "\n";
         }
 
+        ConfigIO WriteConfigIO = new ConfigIO(configFileToWrite);
+        WriteConfigIO.writeToFile(stringToWrite);
     }
 
     public void readConfigFile() {
@@ -86,7 +87,7 @@ public class SaveProjectController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //TODO then add then add the samples to the view!.
+        //TODO then add then add the samples to the view!
 
     }
 
