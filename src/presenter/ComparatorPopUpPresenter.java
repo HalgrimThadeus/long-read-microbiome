10e80e400fa43c9317c394a73f77d2571f8258fe
hannah-frank <hannah.frank@student.uni-tuebingen.de>
2018-07-06 17:01:23 +0200

    init comperator classes

---
+++ src/presenter/ComparatorPopUpPresenter.java
@@ -0,0 +1,4 @@
+package presenter;
+
+public class ComparatorPopUpPresenter {
+}
---
+++ src/view/ComparatorPopUp.java
@@ -0,0 +1,10 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ComparatorPopUp {
+    @FXML
+    private Button onCompareButtonClicked;
+
+}
