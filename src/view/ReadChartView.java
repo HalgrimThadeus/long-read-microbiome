b84af1e5962e9eb86a25439f22491b805ea3b26b
manumanu12345 <manuel.gloeckler@student.uni-tuebingen.de>
2018-07-07 01:29:59 +0200

    Chart changes

--- src/view/ReadChartView.java
+++ src/view/ReadChartView.java
@@ -1,18 +1,30 @@
 package view;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import model.GffEntry;
 import model.Read;
 import model.Sample;
 import model.io.SampleReader;
+import model.io.TaxIO;
+import model.services.ReadInTaxTreeService;
+import model.tax.TaxTree;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -40,27 +52,55 @@ public class ReadChartView implements Initializable {
     @FXML
     public Spinner<Integer> barWidthSpinner;
 
-    @FXML Slider zoomSlider1;
+    @FXML Slider zoomSlider;
 
-    private final int BAR_WIDTH = 20;
+    @FXML
+    AnchorPane readChartPane;
+
+    @FXML
+    ScrollPane scrollPane;
+
+    @FXML
+    ToolBar toolBar;
+
+    private IntegerProperty barWidth = new SimpleIntegerProperty(20);
+    private DoubleProperty zoomFactor = new SimpleDoubleProperty(1);
 
     //For testing
     private List<Read> filteredReads = new ArrayList<>();
+    private TaxTree tree;
+
+    private final int BASIC_WIDTH = 740;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //For testing
         try {
             Sample sample = SampleReader.read("res/ehec/ehec.f1000000-ex.fasta", "res/ehec/ehec.f1000000-all.gff", "res/ehec/ehec.f1000000-readname2taxonid.txt");
-
+            TaxIO tree = new TaxIO("res/TreeDumpFiles/nodesTest.dmp", "res/TreeDumpFiles/namesTest.dmp");
+            this.tree = tree.readInTaxTree();
             choseReadChoiceBox.getItems().addAll(sample.getReads());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(5, 60);
-        barWidthSpinner.setValueFactory(valueFactory);
-        barWidthSpinner.getValueFactory().setValue(BAR_WIDTH);
 
+
+        //Spiner is initialized
+        SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(5, 200);
+        barWidthSpinner.setValueFactory(valueFactory);
+        barWidthSpinner.getValueFactory().setValue((int)barWidth.get());
+        barWidthSpinner.valueProperty().addListener((observable, oldValue, newValue) -> {
+            barWidth.set(newValue);
+        });
+
+        toolBar.prefWidthProperty().bind(scrollPane.widthProperty());
+
+        zoomFactor.bind(zoomSlider.valueProperty());
+
+        readChartPane.prefWidthProperty().bind(zoomFactor.multiply(BASIC_WIDTH));
+        readChartPane.maxWidthProperty().bind(zoomFactor.multiply(BASIC_WIDTH));
+        readChartPane.minWidthProperty().bind(zoomFactor.multiply(BASIC_WIDTH));
 
 
     }
@@ -84,16 +124,15 @@ public class ReadChartView implements Initializable {
 
             //Has do be done like this, because the getDisplayedValue can only be calculated when the xAxis is drawn...
             if(xAxisSetHigherBoundIfNeeded(length)){
-                addSequenceLength(length, oldUpperBound);
+                addSequenceLength(read.getId(), length, oldUpperBound);
                 addGenes(gffEntries, oldUpperBound);
             }
             else{
-                addSequenceLength(length, -1);
+                addSequenceLength(read.getId(), length, -1);
                 addGenes(gffEntries, -1);
             }
 
             addName(read.getTaxonomicId() + "");
-
 
         }
 
@@ -123,20 +162,21 @@ public class ReadChartView implements Initializable {
      * @param length
      * @param oldUpperBound
      */
-    private void addSequenceLength(int length, double oldUpperBound){
+    private void addSequenceLength(String id, int length, double oldUpperBound){
         Rectangle newSequence = new Rectangle();
-        newSequence.setHeight(BAR_WIDTH);
+        newSequence.heightProperty().bind(barWidth);
 
+        //If the xAxis wanna get repaint at the same time u have to calculate it different
         if(oldUpperBound == -1)
             newSequence.setWidth(xAxis.getDisplayPosition(length));
         else{
             newSequence.setWidth(xAxis.getDisplayPosition(length* (oldUpperBound/xAxis.getUpperBound())));
         }
 
-
         newSequence.setFill(Color.GRAY);
         sequences.getChildren().add(newSequence);
 
+        //Right resizing
         xAxis.widthProperty().addListener((obs, oldVal, newVal) -> {
 
             newSequence.setWidth(newSequence.getWidth()* (newVal.doubleValue()/oldVal.doubleValue()));
@@ -146,11 +186,9 @@ public class ReadChartView implements Initializable {
             newSequence.setWidth((oldVal.doubleValue()/newVal.doubleValue()) * newSequence.getWidth());
         });
 
-        barWidthSpinner.valueProperty().addListener(((observable, oldValue, newValue) -> {
-            newSequence.setHeight(newValue);
-        }));
-
-        Tooltip t = new Tooltip("Länge: " + length);
+        //Tooltip
+        //TODO may add sequence id
+        Tooltip t = new Tooltip("Sequence-Id: " + id + "\n" + "Length: " + length);
         Tooltip.install(newSequence, t);
     }
 
@@ -161,43 +199,63 @@ public class ReadChartView implements Initializable {
      */
     private void addGenes(List<GffEntry> gffEntries, double oldUpperBound){
         AnchorPane pane = new AnchorPane();
-        pane.setMaxHeight(BAR_WIDTH);
+        pane.prefHeightProperty().bind(barWidth);
 
         for(int j = 0; j < gffEntries.size(); j++){
+
             int start =gffEntries.get(j).getStart();
             int end = gffEntries.get(j).getEnd();
+            boolean isReversed = (gffEntries.get(j).getStrand() == '+') ? false : true;
+
+
             Rectangle rectangle = new Rectangle();
             if(oldUpperBound == -1){
                 rectangle.setWidth(xAxis.getDisplayPosition(end)-xAxis.getDisplayPosition(start));
                 rectangle.setX(xAxis.getDisplayPosition(start));
+
+
             }
             else{
                 rectangle.setWidth(xAxis.getDisplayPosition(end * (oldUpperBound/xAxis.getUpperBound()))-xAxis.getDisplayPosition(start * (oldUpperBound/xAxis.getUpperBound())));
                 rectangle.setX(xAxis.getDisplayPosition(start * (oldUpperBound/xAxis.getUpperBound())));
             }
 
-            rectangle.setHeight(BAR_WIDTH);
+            LinearGradient lg1;
 
-            rectangle.setFill(Color.rgb(178,34,34,0.5));
+            if(isReversed){
+                Stop[] stops = new Stop[] {new Stop(1, Color.rgb(178,34,34,0.5)), new Stop(0, Color.rgb(34,34,178,0.5))};
+                lg1 = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, stops);
+            }
+            else{
+                Stop[] stops = new Stop[] {new Stop(1, Color.rgb(178,34,34,0.5)), new Stop(0, Color.rgb(34,34,178,0.5))};
+                lg1 = new LinearGradient(1, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
+            }
+
+
+            rectangle.heightProperty().bind(barWidth);
+            rectangle.setFill(lg1);
             rectangle.setStrokeWidth(0.5);
             rectangle.setStroke(Color.BLACK);
 
-            Tooltip t = new Tooltip(gffEntries.get(j).getAttributes().get(" Name"));
-            Tooltip.install(rectangle, t);
 
             xAxis.widthProperty().addListener((obs, oldVal, newVal) -> {
                 rectangle.setWidth(rectangle.getWidth()* (newVal.doubleValue()/oldVal.doubleValue()));
                 rectangle.setX(rectangle.getX() * (newVal.doubleValue()/oldVal.doubleValue()) );
+
             });
 
             xAxis.upperBoundProperty().addListener((obs, oldVal, newVal) -> {
                 rectangle.setWidth(rectangle.getWidth()* (oldVal.doubleValue()/newVal.doubleValue()));
                 rectangle.setX(rectangle.getX() * (oldVal.doubleValue()/newVal.doubleValue()) );
+
             });
 
-            barWidthSpinner.valueProperty().addListener(((observable, oldValue, newValue) -> {
-                rectangle.setHeight(newValue);
-            }));
+            //Add tooltip
+            String tooltipMessage = gffEntries.get(j).getAttributes().get(" Name") + "\n"
+                    + "Start: " + start + " End: " + end + "\n"
+                    + "Strand: " + gffEntries.get(j).getStrand();
+            Tooltip t = new Tooltip(tooltipMessage);
+            Tooltip.install(rectangle, t);
 
 
             pane.getChildren().add(rectangle);
@@ -214,7 +272,8 @@ public class ReadChartView implements Initializable {
     private void addName(String name){
         //TODO Transform them to names add listener to spinner
         Label label = new Label(name);
+        label.prefHeightProperty().bind(barWidth);
         names.getChildren().add(label);
-        names.setSpacing(BAR_WIDTH*1.25);
+        names.spacingProperty().bind(barWidth);
     }
 }
