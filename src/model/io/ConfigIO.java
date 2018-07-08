157c3bffe705266a22d4034c13f5528a49bc5bc2
d-manuel <manuel.dienert@t-online.de>
2018-07-08 23:10:54 +0200

    now option to select filepath for saving

--- src/model/io/ConfigIO.java
+++ src/model/io/ConfigIO.java
@@ -39,16 +39,16 @@ public class ConfigIO {
     public List<Sample> readProjectFromFile() throws Exception{
         List<Sample> samples = new ArrayList<Sample>();
 
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
-            Sample sample;
-            do {
-                sample = (Sample)objectInputStream.readObject();
-                if(sample == null) break;
-                samples.add(sample);
-            }while(sample != null);
+                Sample sample;
+                do {
+                    sample = (Sample)objectInputStream.readObject();
+                    if(sample == null) break;
+                    samples.add(sample);
+                }while(sample != null);
 
             objectInputStream.close();
             fileInputStream.close();
@@ -62,75 +62,5 @@ public class ConfigIO {
         return samples;
     }
 
-
-
-
-
-
-
-
-
-
-    /*
-    private FileWriter writer;
-
-
-    private FileReader reader;
-
-
-    /**
-     * Constructor for Writing and Reading, initialized by passing a File
-     *
-     * @param file
-
-    public ConfigIO(File file) throws IOException {
-        this.writer = new FileWriter(file);
-        this.reader = new FileReader(file);
-    }
-
-
-    public void writeToFile(String stringToWrite) throws Exception {
-
-
-        if (this.writer == null) {
-            throw new Exception("Config-Reader has not been initializied yet");
-        } else {
-            BufferedWriter bufferedWriter = new BufferedWriter(this.writer);
-            bufferedWriter.write(stringToWrite);
-            System.out.println("Succesfully wrote something to file");
-            bufferedWriter.close();
-        }
-    }
-
-    /**
-     * TODO until now should just return listofPaths
-     * @param configFile
-     * @return
-     * @throws Exception
-
-    public List<String[]> readInConfigFile(File configFile) throws Exception {
-        List<String[]> listOfListOfPaths = new ArrayList<String[]>();
-        if (this.reader == null) {
-            throw new Exception("CSV-Reader has not been initializied yet");
-        } else {
-            BufferedReader bufferedReader = new BufferedReader(this.reader);
-
-            String currentLine = "";
-            int positionInArray = 0;
-            int positionInList = 0;
-            while ((currentLine = bufferedReader.readLine()) != null ) {
-                if(currentLine.startsWith("#")){
-                   positionInList++;
-                   listOfListOfPaths.add(new String[3]);
-                }
-                else{
-                    listOfListOfPaths.get(positionInList)[positionInList] = currentLine;
-                    positionInArray++;
-                }
-            }
-            return listOfListOfPaths;
-        }
-    }
-    */
 
 }
--- src/presenter/SaveProjectPresenter.java
+++ src/presenter/SaveProjectPresenter.java
@@ -11,6 +11,7 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.*;
 import java.lang.reflect.Array;
@@ -29,7 +30,12 @@ public class SaveProjectPresenter {
 
     public void saveProject() throws Exception {
         //TODO get the file from the filebrowser located in the VIEW package
-        File file = new File("savedProject.project");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Project to File...");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Project", "*.project"));
+        fileChooser.setInitialFileName("save01.project");
+        File file = fileChooser.showSaveDialog(new Stage());
 
         ConfigIO configIO = new ConfigIO(file);
         List<Sample> nonObservedSampleList =  project.getSamples();
@@ -50,100 +56,4 @@ public class SaveProjectPresenter {
         }
 
 
-    }
-
-
-        /*//get the list of samples and the filepaths of the samples
-
-        List<Sample> listOfSamples = new ArrayList<>();
-
-        List<File[]> listOfPaths = new ArrayList<>();
-
-
-        File configFileToWrite = createEmptyFile();
-
-        saveFilePathToFile(listOfSamples, listOfPaths, configFileToWrite);
-
-    }
-
-    private File createEmptyFile() {
-        File fileToWrite = null;
-
-        //choose the save-directory
-        int configVersionName = 1;
-        JFileChooser f = new JFileChooser();
-        DirectoryChooser dc = new DirectoryChooser();
-        dc.showDialog(new Stage());
-        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        f.showSaveDialog(null);
-        System.out.println(f.getSelectedFile());
-
-        //make a config file, that doesnt exist already
-        do {
-            String fileToWritePath = f.getSelectedFile() + "\\config" + configVersionName + ".lrcfg";
-
-            fileToWrite = new File(fileToWritePath);
-
-            configVersionName++;
-
-        } while (fileToWrite.isFile());
-
-        return fileToWrite;
-
-    }
-
-    //Information: FileFormat *.lrcfg stands for "long read config File".
-    public void saveFilePathToFile(List<Sample> listOfSamples, List<File[]> listOfPaths, File configFileToWrite) throws Exception {
-        String stringToWrite = "";
-
-        for (int i = 0; i < listOfPaths.size(); i++) {
-            //create the StringFilePath arrays (with the 3 filepaths of fasta, gff, and csv)
-
-            File[] file = listOfPaths.get(i);
-
-            String fastaPath = file[0].getAbsolutePath();
-            String gffPath = file[1].getAbsolutePath();
-            String csvPath = file[2].getAbsolutePath();
-
-            stringToWrite += "#Sample" + i + "\n";
-            stringToWrite += fastaPath + "\n";
-            stringToWrite += gffPath + "\n";
-            stringToWrite += csvPath + "\n";
-        }
-
-        ConfigIO WriteConfigIO = new ConfigIO(configFileToWrite);
-        WriteConfigIO.writeToFile(stringToWrite);
-    }
-
-    public void readConfigFile() throws Exception {
-        //first get the new File from FileBrowser:
-        File configFile = getNewFile();
-
-        //then read the file
-        try {
-            ConfigIO readConfigIO = new ConfigIO(configFile);
-
-            readConfigIO.readInConfigFile(configFile);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //TODO then add then add the samples to the view!
-
-    }
-
-
-    public File getNewFile() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Config File");
-        fileChooser.getExtensionFilters().add(
-                new FileChooser.ExtensionFilter("Config", "*.lrcfg")
-        );
-
-        File selectedFile = fileChooser.showOpenDialog(new Stage());
-
-        return selectedFile;
-    }
-
-*/
-}
+    }}
