824aba5dc5a6e4b96d2ab3370e025edb3dfd975b
manumanu12345 <manuel.gloeckler@student.uni-tuebingen.de>
2018-07-19 20:06:23 +0200

    Zoom

--- src/presenter/ReadChartViewPresenter.java
+++ src/presenter/ReadChartViewPresenter.java
@@ -80,8 +80,10 @@ public class ReadChartViewPresenter {
 
         for(Read read : reads){
             for(GffEntry gffEntry : read.getGFFEntries()){
-                if(name.contains(gffEntry.getAttributes().get(" Name"))){
-                    readByName.add(read);
+                if(gffEntry.getAttributes().containsKey(" Name")){
+                    if(gffEntry.getAttributes().get(" Name").contains(name)){
+                        readByName.add(read);
+                    }
                 }
             }
         }
@@ -153,7 +155,6 @@ public class ReadChartViewPresenter {
 
             Rectangle rectangle = new Rectangle();
 
-            //TODO Make search gen work again
 
             //Genes with start reds the get blue
             if (isReversed) {
@@ -248,12 +249,16 @@ public class ReadChartViewPresenter {
         //TODO isGen filter is not working proper
 
         List<Read> reads = getReadsByName(filteredSample.getFilteredReads(), genName);
+
+        getZoomAreaReads(readChartView, reads, genName);
     }
 
 
     private int AREA_WIDTH = 1000;
 
     private void getZoomAreaReads(ReadChartView readChartView, List<Read> reads, String genName) {
+
+        readChartView.xAxis.setUpperBound(2000);
 
         for (Read read : reads) {
 
@@ -265,17 +270,135 @@ public class ReadChartViewPresenter {
             //If there are more then one, always the first is taken
             int startGen = Integer.MAX_VALUE;
             for (GffEntry gens : read.getGFFEntries()) {
-                if (gens.getStart() < startGen && genName.contains(gens.getAttributes().get(" Name"))) {
-                    startGen = gens.getStart();
+                if(gens.getAttributes().containsKey( " Name")) {
+                    if (gens.getStart() < startGen && gens.getAttributes().get(" Name").contains(genName)) {
+                        startGen = gens.getStart();
+                    }
                 }
             }
+
             //Area width, where genes are painted
             int lowerBound = startGen - AREA_WIDTH;
             int upperBound = startGen + AREA_WIDTH;
 
-            //readChartView.reads.add(getSequence(read));
+            AnchorPane[] genes = addZoomedGenes(read.getGFFEntries(), lowerBound, startGen, upperBound);
+
+            readChartView.genPane.add(genes[0]);
+            readChartView.reversedGenPane.add(genes[1]);
+            readChartView.name.add(addName(taxId));
+            readChartView.reads.add(getZoomedSequence(read));
 
         }
+        readChartView.xAxis.setLowerBound(-1000);
+        readChartView.xAxis.setUpperBound(1000);
+        readChartView.xAxis.setTickUnit(100);
+    }
+
+    private Rectangle getZoomedSequence(Read read){
+        String id = read.getId();
+
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.setFill(Color.GRAY);
+        rectangle.widthProperty().bind(readChartView.xAxis.widthProperty());
+        rectangle.heightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+
+        Tooltip t = new Tooltip("Sequence-Id: " + id + "\n" + "Length: " + read.getSequence().length());
+        Tooltip.install(rectangle, t);
+
+        return rectangle;
+    }
+
+    private AnchorPane[] addZoomedGenes(List<GffEntry> gffEntries, int lowerBound, int startGen, int upperBound){
+
+        AnchorPane genes = new AnchorPane();
+        AnchorPane genesReversed = new AnchorPane();
+
+
+        genes.prefHeightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+        genes.minHeightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+        genesReversed.prefHeightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+        genesReversed.minHeightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+
+        for (GffEntry gffEntry : gffEntries) {
+
+            String name = "" + gffEntry.getAttributes().get(" Name");
+            int start = gffEntry.getStart();
+            int end = gffEntry.getEnd();
+            boolean isReversed = (gffEntry.getStrand() == '+') ? false : true;
+
+            System.out.println(start);
+            System.out.println(startGen);
+
+            if(start >= startGen-AREA_WIDTH && start <= startGen+AREA_WIDTH){
+                start = start - startGen + AREA_WIDTH;
+                if(start < 0){
+                    start = 0;
+                }
+                if(end > upperBound) {
+                    end = 2*AREA_WIDTH;
+                }else{
+                    end = end-startGen + AREA_WIDTH;
+                }
+                System.out.println(start);
+                System.out.println(end);
+            }
+            else{
+                continue;
+            }
+
+
+            Rectangle rectangle = new Rectangle();
+
+            //Genes with start reds the get blue
+            if (isReversed) {
+                rectangle.setFill(javafx.scene.paint.Color.rgb(34, 34, 178, 0.5));
+                isSearched.addListener((observable, oldValue, newValue) -> {
+
+                    if (!name.contains(searchedGene)) {
+                        rectangle.setFill(Color.rgb(34, 34, 178, 0.5));
+                    } else {
+                        rectangle.setFill(Color.rgb(255, 215 , 0, 0.5));
+                    }
+                });
+                genesReversed.getChildren().add(rectangle);
+
+            } else {
+                rectangle.setFill(javafx.scene.paint.Color.rgb(178, 34, 34, 0.5));
+                isSearched.addListener((observable, oldValue, newValue) -> {
+                    if (!name.contains(searchedGene)) {
+                        rectangle.setFill(Color.rgb(178, 34, 34, 0.5));
+                    } else {
+                        rectangle.setFill(Color.rgb(255, 215 , 0, 0.5));
+                    }
+                });
+                genes.getChildren().add(rectangle);
+            }
+
+
+            rectangle.heightProperty().bind(readChartView.barWidthSpinner.valueProperty());
+            rectangle.setStrokeWidth(0.5);
+            rectangle.setStroke(Color.BLACK);
+            rectangle.xProperty().bind(readChartView.xAxis.widthProperty().divide(readChartView.xAxis.upperBoundProperty()).multiply(start).multiply(9.5));
+            rectangle.widthProperty().bind(readChartView.xAxis.widthProperty().divide(readChartView.xAxis.upperBoundProperty()).multiply(end-start).multiply(9.5));
+
+            //Add tooltip
+            String tooltipMessage = name + "\n"
+                    + "Start: " + start + " End: " + end + "\n"
+                    + "Strand: " + ((isReversed) ? "+" : "-");
+            Tooltip t = new Tooltip(tooltipMessage);
+            Tooltip.install(rectangle, t);
+
+
+            //genes.getChildren().add(rectangle);
+
+        }
+
+        //sequences.getChildren().add(pane);
+        AnchorPane[] allGenes = new AnchorPane[2];
+        allGenes[0] = genes;
+        allGenes[1] = genesReversed;
+        return allGenes;
     }
 
 
